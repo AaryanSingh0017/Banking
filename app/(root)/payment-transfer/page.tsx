b8764278051661b76a97fbf0e1bad62b10f08ddf
AaryanSingh0017 <aaryansingh0017@gmail.com>
2024-08-07 07:54:04 +0530

    Implement auth,appwrite and sentry

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,8 +1,11 @@
+import AuthForm from '@/components/ui/AuthForm'
 import React from 'react'
 
 const Tranfer = () => {
   return (
-    <div>Tranfer</div>
+    <section className='flex-center size-full max-sm:px-6'>
+        <AuthForm type="sign-in" />
+    </section>
   )
 }
 
---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://5c3a26925196502fcc6d838b13d98785@o4507733566554112.ingest.de.sentry.io/4507733577695312",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
